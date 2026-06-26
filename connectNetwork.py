2d269fc7d48f461ea3d2c434aee44f1db9bd9608
apurvaTeacher <agaygole@gmail.com>
2026-06-26 13:29:57 +0530

    send command code added

---
+++ connectNetwork.py
@@ -0,0 +1,13 @@
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "10.10.10.1",
+    "username": "sam",
+    "password": "cisco123",
+    "port": 22,
+}
+
+with ConnectHandler(**device) as conn:
+    output = conn.send_command("show ip int br")
+    print(output)
